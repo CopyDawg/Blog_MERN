56c12c6e0172e879f40086b41b87ab5ac72ecf01
Omar <omarcory2@gmail.com>
2023-10-20 04:54:15 -0600

    first commit

---
+++ client/src/components/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div className="post">
+            <div className="image">
+                <img src="https://st3.depositphotos.com/3203307/18861/i/450/depositphotos_188618866-stock-photo-computer-malware-attack.jpg"/>
+            </div>
+            <div className="text">
+                <h2>September 2023: Major Cyber Attacks, Data Breaches, Ransomware Attacks</h2>
+                <p className="info"> 
+                    <a href="#">@CopyDawg</a>  
+                    <time>20-10-2023 02:10</time>
+                </p>
+                <p>The Biggest Cyber Attacks, Ransomware Attacks and Data Breaches - our compilation for the month of September 2023. </p>
+            </div>
+        </div>
+    );
+}
