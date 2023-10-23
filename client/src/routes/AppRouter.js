f75ea0f621c9cf6685f1992b6eb0fcff5f613f5c
Omar <omarcory2@gmail.com>
2023-10-23 01:44:00 -0600

    added layout

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -1,13 +1,16 @@
 import "../App.css";
 import { Route, Routes } from "react-router-dom";
 import { HomePage, LoginPage, RegisterPage } from "../pages";
+import { Layout } from "../layout/Layout";
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={ <HomePage/> } />
-            <Route path="/login" element={ <LoginPage/> } />
-            <Route path="/join" element={ <RegisterPage/> } />
+            <Route path="/" element={<Layout/>}>
+                <Route index element={ <HomePage/> } />
+                <Route path="/login" element={ <LoginPage/> } />
+                <Route path="/join" element={ <RegisterPage/> } />
+            </Route>
         </Routes>
     )
 }
